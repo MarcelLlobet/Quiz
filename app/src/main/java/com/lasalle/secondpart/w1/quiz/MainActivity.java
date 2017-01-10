d9c583249f28117c6ff92bc48ecde20fea350a0d
MarcelLlobet <ls25294@salleurl.edu>
2017-01-10 21:54:49 +0100

    Fin classe 1

--- app/src/main/java/com/lasalle/secondpart/w1/quiz/MainActivity.java
+++ app/src/main/java/com/lasalle/secondpart/w1/quiz/MainActivity.java
@@ -2,12 +2,38 @@ package com.lasalle.secondpart.w1.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startQuiz = (Button) findViewById(R.id.buttonStart);
+
+        startQuiz.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                System.out.println("Start");
+                Intent intent = new Intent(getApplicationContext(),questions.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
